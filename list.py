6f625b3fbbe23fa83687cbaa79738b14afc27f81
karthikchary <karthik.chary138@gmail.com>
2022-08-04 11:27:15 +0530

    logic working

--- list.py
+++ list.py
@@ -1,14 +1,17 @@
 
 skills = {"devops":("jenkins","Ansible","Docker","Terraform"),"Development":["dev","sit","UAT"]}
-for skill in skills:
-    print(skill[0][2]) 
+# for skill in skills:
+#     print(skill[0][2]) 
 
 print("karthik")
 
 
 
 
-
+x=2
+y=3
+total = x+y
+print(total)
 
 
 
