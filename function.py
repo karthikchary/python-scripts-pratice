731e3c6f8b757ddf393fc0d9b7931e20be2a26fb
karthikchary <karthik.chary138@gmail.com>
2022-08-04 14:47:51 +0530

    functions added

--- function.py
+++ function.py
@@ -1,25 +1,27 @@
 #!/usr/bin/python3
 import os
+def useradd():
+    userlist = ["alpha","beta","gamma"]
+    for user in userlist:
+        exitcode = os.system(("id %s") % user)
+        if exitcode == 0:
+            print("User already exists")
+        else:
+            os.system(("useradd %s") % user)
 
-userlist = ["alpha","beta","gamma"]
-for user in userlist:
-    exitcode = os.system(("id %s") % user)
-    if exitcode == 0:
-        print("User already exists")
+    exitcode = os.system("grep science /etc/group")
+    if exitcode != 0:
+        os.system("groupadd science")
     else:
-        os.system(("useradd %s") % user)
+        print("Group already exists")
 
-exitcode = os.system("grep science /etc/group")
-if exitcode != 0:
-    os.system("groupadd science")
-else:
-    print("Group already exists")
+    for user in userlist:
+        os.system(("usermod -G science %s") % user)
 
-for user in userlist:
-    os.system(("usermod -G science %s") % user)
+def directory():
+    os.mkdir("/opt/science_directory")
+    os.system("chown :science /opt/science_directory")
+    os.system("chmod 770 /opt/science_directory")
 
-os.mkdir("/opt/science_directory")
-
-os.system("chown :science /opt/science_directory")
-
-os.system("chmod 770 /opt/science_directory")
+useradd()
+directory()
