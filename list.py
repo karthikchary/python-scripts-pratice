9be93f6fa83ec90af2bf447b825b240d6a4df7b9
karthikchary <karthik.chary138@gmail.com>
2022-08-04 11:16:50 +0530

    added few more lines

--- list.py
+++ list.py
@@ -1,4 +1,18 @@
-# accdscs
-# sdvscv
 
-print("Karthik hi")
+skills = {"devops":("jenkins","Ansible","Docker","Terraform"),"Development":["dev","sit","UAT"]}
+for skill in skills:
+    print(skill[0][2]) 
+
+print("karthik")
+
+
+
+
+
+
+
+
+
+
+
+
