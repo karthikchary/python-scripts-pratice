3a06f76ec7d2a980c1e8a78583ed2a065926af69
karthik chary <karthik.chary138@gmail.com>
2022-08-04 11:35:27 +0000

    updated code

--- fab_scripts/python3-virtualenv/fabfiles/fabfile.py
+++ fab_scripts/python3-virtualenv/fabfiles/fabfile.py
@@ -1,7 +1,10 @@
+import warnings
+from cryptography.utils import CryptographyDeprecationWarning
+warnings.filterwarnings("ignore", category=CryptographyDeprecationWarning)
 from fabric.api import *
 
 def msg():
-    print "Good Morning"
+    print("Good Morning.")
 
 def disk_space():
     local("df -h")
